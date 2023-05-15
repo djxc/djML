f56bde77043125f0dc462cdd375be34a46e668a3
djxc <dj_jxc@163.com>
2023-05-15 15:21:18 +0800

    调整resnet全连接层增加dropout

--- project/video_feature_encode/model.py
+++ project/video_feature_encode/model.py
@@ -81,6 +81,72 @@ class LeNet(nn.Module):
         output = self.fc(feature)
         return output
     
+class Inception(nn.Module):
+    # c1--c4是每条路径的输出通道数
+    def __init__(self, in_channels, c1, c2, c3, c4, **kwargs):
+        super(Inception, self).__init__(**kwargs)
+        # 线路1，单1x1卷积层
+        self.p1_1 = nn.Conv2d(in_channels, c1, kernel_size=1)
+        # 线路2，1x1卷积层后接3x3卷积层
+        self.p2_1 = nn.Conv2d(in_channels, c2[0], kernel_size=1)
+        self.p2_2 = nn.Conv2d(c2[0], c2[1], kernel_size=3, padding=1)
+        # 线路3，1x1卷积层后接5x5卷积层
+        self.p3_1 = nn.Conv2d(in_channels, c3[0], kernel_size=1)
+        self.p3_2 = nn.Conv2d(c3[0], c3[1], kernel_size=5, padding=2)
+        # 线路4，3x3最大汇聚层后接1x1卷积层
+        self.p4_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        self.p4_2 = nn.Conv2d(in_channels, c4, kernel_size=1)
+
+    def forward(self, x):
+        p1 = F.relu(self.p1_1(x))
+        p2 = F.relu(self.p2_2(F.relu(self.p2_1(x))))
+        p3 = F.relu(self.p3_2(F.relu(self.p3_1(x))))
+        p4 = F.relu(self.p4_2(self.p4_1(x)))
+        # 在通道维度上连结输出
+        return torch.cat((p1, p2, p3, p4), dim=1)
+
+
+class GoogLeNet(nn.Module):
+    def __init__(self, in_channels, class_num) -> None:
+        super(GoogLeNet, self).__init__()
+        self.b1 = nn.Sequential(
+                nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+            )
+        self.b2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 192, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.b3 = nn.Sequential(
+            Inception(192, 64, (96, 128), (16, 32), 32),
+            Inception(256, 128, (128, 192), (32, 96), 64),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.b4 = nn.Sequential(
+            Inception(480, 192, (96, 208), (16, 48), 64),
+            Inception(512, 160, (112, 224), (24, 64), 64),
+            Inception(512, 128, (128, 256), (24, 64), 64),
+            Inception(512, 112, (144, 288), (32, 64), 64),
+            Inception(528, 256, (160, 320), (32, 128), 128),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        
+        self.b5 = nn.Sequential(
+            Inception(832, 256, (160, 320), (32, 128), 128),
+            Inception(832, 384, (192, 384), (48, 128), 128),
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Flatten()
+        )
+        self.net = nn.Sequential(self.b1, self.b2, self.b3, self.b4, self.b5, nn.Linear(1024, class_num))
+    
+    def forward(self, x):
+        return self.net(x)
 
 def create_net(net_name: str, class_num: int, resume=""):
     """根据模型名称创建模型
@@ -115,9 +181,10 @@ def create_net(net_name: str, class_num: int, resume=""):
         num_ftrs = net.fc.in_features
         # 修改最后的全连接层
         net.fc = nn.Sequential(
-            nn.Linear(num_ftrs, 512),
+            nn.Linear(num_ftrs, 64),
             nn.ReLU(),
-            nn.Linear(512, class_num)
+            nn.Dropout(),
+            nn.Linear(64, class_num)
         )
     elif net_name == "resnext":
         net = torchvision.models.resnext50_32x4d(pretrained=True)
