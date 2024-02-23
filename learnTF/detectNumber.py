11adafa76e231209795df25e69d5e47517ba5f71
djxc <dj_jxc@163.com>
2024-02-23 14:11:25 +0800

    裁剪影像数据

--- learnTF/detectNumber.py
+++ learnTF/detectNumber.py
@@ -4,20 +4,22 @@
 # 使用tensorflow识别手写数字，mnist
 
 import os
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers, optimizers, datasets
+from keras import layers, optimizers, datasets
 
 def load_data():
     '''加载数据，对原始数据以及标签进行初步调整'''
     (x, y), (x_val, y_val) = datasets.mnist.load_data()
     # 原始数据为灰度图值在0-255，这里将其缩放在-1 ~ 1之间
-    x = 2 * tf.convert_to_tensor(x, dtype=tf.float32)/255. - 1      
+    x = 2 * tf.convert_to_tensor(x, dtype=tf.float32)/255. - 1     
+    x = np.expand_dims(x, axis=3)
     y = tf.convert_to_tensor(y, dtype=tf.int32)
     y = tf.one_hot(y, depth=10)
     print(x.shape, y.shape)
     train_dataset = tf.data.Dataset.from_tensor_slices((x, y))
-    train_dataset = train_dataset.batch(200)
+    train_dataset = train_dataset.batch(320)
     return train_dataset
 
 def create_model():
@@ -32,7 +34,24 @@ def create_model():
     optimizer = optimizers.SGD(learning_rate=0.001)
     return model, optimizer 
 
-def train_model(model, train_dataset, optimizer, epoch_num):
+def create_cnn_model():
+    """"""
+    model = keras.Sequential(
+        [
+            layers.Conv2D(kernel_size=3, filters=16, activation='relu'),
+            layers.MaxPool2D(pool_size=2, strides=2),
+            layers.Conv2D(kernel_size=3, filters=32, activation='relu'),
+            layers.MaxPool2D(pool_size=2, strides=2),
+            layers.Conv2D(kernel_size=3, filters=32, activation='relu'),
+            layers.Flatten(),
+            layers.Dense(64, activation='relu'),
+            layers.Dense(10, activation='softmax'),
+        ]
+    )
+    optimizer = optimizers.RMSprop(learning_rate=0.001)
+    return model, optimizer
+
+def train_model(model: keras.Sequential, train_dataset, optimizer, epoch_num):
     '''
     1、根据循环次数，循环train次数
     2、train_dataset为一个batch，遍历每一数据，利用模型计算输出；
@@ -41,10 +60,11 @@ def train_model(model, train_dataset, optimizer, epoch_num):
     for epoch in range(epoch_num):
         for step, (x, y) in enumerate(train_dataset):
             with tf.GradientTape() as tape:
-                x = tf.reshape(x, (-1, 28*28))  # 将二维数组拉平 [b, 28, 28] => [b, 784]
+                # x = tf.reshape(x, (-1, 28*28))  # 将二维数组拉平 [b, 28, 28] => [b, 784]
                 out = model(x)                  # 计算预测输出，[b, 784] => [b, 10]
                 # loss = tf.reduce_sum(tf.square(out - y)) / x.shape[0]   # Step2. compute loss
-                loss = tf.reduce_mean(tf.keras.losses.mse(y, out))
+                
+                loss = tf.reduce_mean(tf.keras.losses.categorical_crossentropy(y, out))
             # Step3. optimize and update w1, w2, w3, b1, b2, b3
             grads = tape.gradient(loss, model.trainable_variables)
             # w' = w - lr * grad
@@ -83,5 +103,5 @@ def diy_trian(x, y_onehot, lr):
 
 if __name__ == "__main__":
     train_dataset = load_data()
-    model, optimizer = create_model()
+    model, optimizer = create_cnn_model()
     train_model(model, train_dataset, optimizer, 50)
